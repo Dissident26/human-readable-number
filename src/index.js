c1111776451f120b96ef3a5555f48b27413e815b
Dissident26 <60570678+Dissident26@users.noreply.github.com>
2020-03-17 16:33:42 +0300

    feat: add solution
    add solution for human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let words = [
+        ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen'],
+        ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine']
+    ];
+
+    if ( number < 15 ) {
+        return words[0][number]
+    } else if ( number < 20 ) {
+        return words[1][Math.floor( number - 10 ) - 2] + 'teen';
+    } else if ( number < 100) {
+        return ( number % 10 == 0 ) ? words[1][Math.floor( number / 10 ) - 2] + 'ty' : words[1][Math.floor( number / 10 ) - 2] + 'ty' + " " + words[0][number % 10];
+    } else {
+        return ( number % 100 == 0 ) ? words[0][Math.floor( number / 100 )] + ' ' + 'hundred' :
+        words[0][Math.floor( number / 100 )] + ' ' + 'hundred' + " " + toReadable( number % 100 );
+    }
+};
